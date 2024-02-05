f55ee159971486734aac6cfeba6f021a0d7c4a85
Aditya More <99179330+AdityaMore7000@users.noreply.github.com>
2024-02-05 07:29:30 +0000

    start 2nd

---
+++ Unique values es6/app.js
@@ -0,0 +1,53 @@
+/**
+ * @typedef {Object} Item 
+ * @property {String} name
+ * @property {String} type
+ */
+
+/**
+ * @param {Item[]} items
+ */
+const items = [
+    {
+        name:"Apple",
+        type:"Fruit"
+    },
+    {
+        name:"Orange",
+        type:"Fruit"
+    },
+    {
+        name:"Carrot",
+        type:"Vegetable"
+    },
+    {
+        name:"Tomato",
+        type:"Vegetable"
+    },
+    {
+        name:"Cucumber",
+        type:"Vegetable"
+    },
+    {
+        name:"Banana",
+        type:"Fruit"
+    },
+    {
+        name:"Pineapple",
+        type:"Fruit"
+    },
+    {
+        name:"Strawberry",
+        type:"Fruit"
+    },
+    {
+        name:"Lettuce",
+        type:"Vegetable"
+    },
+    {
+        name:"Blueberry",
+        type:"Fruit"
+    }
+]
+
+console.log(items[0].name)
