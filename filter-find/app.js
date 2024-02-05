4416412571c9860378865bdcb995a9f337e62108
adityamore7000 <adi.more7000@gmail.com>
2024-02-05 15:40:57 +0530

    filter find done

---
+++ filter-find/app.js
@@ -0,0 +1,26 @@
+/**
+ * @typedef {Object} Person
+ * @property {string} name
+ * @property {number} age
+ * @property {string} position
+ */
+
+/**
+ * @param {Person[]} people
+ */
+
+const people = [
+    { name: 'John', age: 20, position: 'developer' },
+    { name: 'Jane', age: 30, position: 'designer' },
+    { name: 'Jack', age: 40, position: 'manager' },
+    { name: 'Jill', age: 50, position: 'developer' },
+    { name: 'James', age: 60, position: 'designer' },
+]
+
+const youngPeople = people.filter((person)=>person.age<=30)
+
+const developers = people.filter((person)=>person.position==='developer')
+// console.log(developers)
+
+const jane = people.filter((person)=>person.name==='Jane')
+console.log(jane)
